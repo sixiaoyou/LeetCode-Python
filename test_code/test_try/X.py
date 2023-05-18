b984fe95a9bfad148a233a884d8e4c8187648e2e
sixiaoyou <yunshaliying@sina.com>
2023-05-18 12:07:59 +0800

    IMIN

--- test_code/test_try/X.py
+++ test_code/test_try/X.py
@@ -3,20 +3,28 @@
 # @Author: sixiaoyou
 # @Email: 834628301@qq.com
 # @File: X.py
+import time
 
 import numpy
 
 
-
 class X:
     def test_explore(self):
-        a = [1, 2]
-        b = [3, 2]
+        l = []
+        for i in range(5000000):
+            l.append(-1)
+        l.append(0)
+        for j in range(5000000):
+            l.append(-1)
 
+        start = time.time()
+        # l = list(filter(lambda x: x!=-1, l))
+        for j in range(10000000):
+            l.remove(-1)
+        print(time.time() - start)
+        return l
 
-        print(list(a & b))
 
 
 x = X()
-x.test_explore()
-
+print(x.test_explore())
