5d510ce3bfcb468df8551df81ef7092b9512b381
sixiaoyou <yunshaliying@sina.com>
2023-05-20 10:19:15 +0800

    What English letter do you think is the greatest in the world?

--- test_code/test_try/X.py
+++ test_code/test_try/X.py
@@ -9,21 +9,8 @@ import numpy
 
 
 class X:
-    def test_explore(self):
-        l = []
-        for i in range(5000000):
-            l.append(-1)
-        l.append(0)
-        for j in range(5000000):
-            l.append(-1)
-
-        start = time.time()
-        # l = list(filter(lambda x: x!=-1, l))
-        for j in range(10000000):
-            l.remove(-1)
-        print(time.time() - start)
-        return l
-
+        def test_explore(self):
+            print(chr(ord('c') - 32))
 
 
 x = X()
