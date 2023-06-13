4602a3468fa976138cc5f23c2090de592c9caacf
sixiaoyou <yunshaliying@sina.com>
2023-06-13 11:55:37 +0800

    0 + 1 == 1 + 0

--- test_code/test_try/X.py
+++ test_code/test_try/X.py
@@ -1,4 +1,4 @@
-# -*-coding: utf-8-*-
+# encoding: utf-8
 # @Time  :2023/6/1 15:04
 # @Author: sixiaoyou
 # @Email: 834628301@qq.com
@@ -8,26 +8,12 @@ import time
 
 
 class X:
-        def test_explore(self, l):
-            c1, c2 = 0, 0
-            p = len(l)
+        def test_explore(self):
+            m = {1,2}
+            print(m.add(2))
 
-            for j in l:
-                if j != -1:
-                    c1 += 1
-                else:
-                    c2 += 1
 
-                if c1 == p:
-                    p = j + 1
-                    break
 
-l = [0]
-n = 82002
-for i in range(n):
-    l.append(-1)
 
 x = X()
-start = time.time()
-print(x.test_explore(l))
-print(time.time() - start)
+x.test_explore()
