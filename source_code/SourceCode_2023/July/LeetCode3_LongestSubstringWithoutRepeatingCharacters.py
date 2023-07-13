a2b61e973166d0fc78e599b1aefc896a9e72e9cf
sixiaoyou <yunshaliying@sina.com>
2023-07-13 14:34:13 +0800

    If the string just has the same character, no matter how long it is, the result is the shortest.

---
+++ source_code/SourceCode_2023/July/LeetCode3_LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,51 @@
+# -*-coding: utf-8-*-
+# @Time  :2023/7/13 14:27
+# @Author: sixiaoyou
+# @Email: 834628301@qq.com
+# @File: LeetCode3_LongestSubstringWithoutRepeatingCharacters.py
+
+'''
+[3. Longest Substring Without Repeating Characters]
+Given a string s, find the length of the longest
+substring
+ without repeating characters.
+
+
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+'''
+
+
+class Solution:
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        ss = ""
+        res = 0
+        for i in range(len(s)):
+            if s[i] in ss:
+                ss = ss[ss.index(s[i]) + 1:]
+            ss += s[i]
+            res = max(res, len(ss))
+        return res
+
